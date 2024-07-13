fb2a0659bf2aec299321ee11726e3fc56d968310
Barath <barathkumar.loyltwo3ks@gmail.com>
2024-07-13 15:43:48 +0530

    first commit

--- app/src/main/java/com/loyltyworks/testproject/MainActivity.kt
+++ app/src/main/java/com/loyltyworks/testproject/MainActivity.kt
@@ -2,10 +2,14 @@ package com.loyltyworks.testproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.loyltyworks.mylibrary.Multiple
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("dfafdsa",""+Multiple.cross(1,3))
     }
 }
